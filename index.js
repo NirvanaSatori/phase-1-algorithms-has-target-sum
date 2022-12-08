b4b19219a2437a3c4e334f5135f6c378e8986650
MORRIS <morris24kariuki@gmail.com>
2022-12-08 23:21:55 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  //iterate through the array ,adding each number to other numbers and return 
+// // take one nums from the array
+      for (let i = 0; i < array.length; i++) {
+        // look through the rest of the array
+        for (let j = i + 1; j < array.length; j++) {
+          // if they add up to the target
+          if (array[i] + array[j] === target) {
+            return true;  //return true
+          }
+        }
+      }
+      return false
 }
-
-/* 
-  Write the Big O time complexity of your function here
+ /* 
+  //Write the Big O time complexity of your function here
 */
 
 /* 
@@ -13,6 +24,10 @@ function hasTargetSum(array, target) {
 /*
   Add written explanation of your solution here
 */
+// take one nums from the array
+//   compare it to the other nums in the array
+//     if they add up to the target
+//       return true
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
